82fd821da600ac2d6d04d3e9816c2d643a873148
Borna Cvitanić <borna.cvitanic@gmail.com>
2024-07-30 23:09:15 +0200

    Update capture.ts to add Clone implementation

--- src/network/capture.rs
+++ src/network/capture.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 use windivert::layer::NetworkLayer;
 use windivert::packet::WinDivertPacket;
 
+#[derive(Clone)]
 pub struct PacketData<'a> {
     pub packet: WinDivertPacket<'a, NetworkLayer>,
     pub arrival_time: Instant,
